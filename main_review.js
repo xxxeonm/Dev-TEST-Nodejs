3f98b4009a901b3a9fa15db54cc1d9a179c5a368
xxxeonm <xeonmin0215@naver.com>
2020-06-17 16:09:23 +0900

    DELETE useless variable

--- main_review.js
+++ main_review.js
@@ -1,7 +1,6 @@
 var http = require('http'); //node의 http모듈을 가져옴, http: http 통신할 때 사용
 var fs = require('fs'); //node의 fs모듈을 가져옴, fs: file system에 접근할 때 사용
 var app = http.createServer(function(request,response){ //서버 생성
-    var url = request.url; //클라이언트가 요청한 URL을 받아옴
     if (request.url == '/'){ //클라이언트가 요청한 URL이 root이면,
         response.writeHead(200); //status code == 200을 보내줌
         response.end("Success"); //index.html을 화면에 보여줌
